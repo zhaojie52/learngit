c31a0da0705f7f8968c36c2bceefe671847ca1eb
zj <1750058707@qq.com>
2017-12-13 09:54:58 +0800

    jieya

---
+++ untitled-2.py
@@ -0,0 +1,18 @@
+#/usr/bin/python
+#coding=utf-8
+import os,sys,time
+import zipfile
+filename = 'my.zip'  #要解压的文件
+filedir = 'C:\zz\ff'  #解压后放入的目录
+r = zipfile.is_zipfile(filename)
+if r:
+    starttime = time.time()
+    fz = zipfile.ZipFile(filename,'r')
+    for file in fz.namelist():
+        print(file)  #打印zip归档中目录
+        fz.extract(file,filedir)
+    endtime = time.time()
+    times = endtime - starttime
+else:
+    print('This file is not zip file')
+print('times' + str(times))
